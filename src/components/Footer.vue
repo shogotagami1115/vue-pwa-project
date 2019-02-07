c6787ce24d34f31210a69d7dd173ae3c284de84d
tagamishogo <tagami@jiraffe.co.jp>
2019-02-07 13:14:08 +0900

    firebase deploy (うごかない)

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Footer">
+    <p><router-link to="/">TOPへ戻る</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+p {
+  margin-top: 5rem;
+}
+</style>

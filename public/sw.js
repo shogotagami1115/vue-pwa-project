244a983f61c8ffab8c1e69f23f61a72d23082153
sumioku_r <rikuson298@gmail.com>
2019-02-11 19:27:44 +0900

    キャッシュの対象を変更

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,10 @@
 
-const CACHE_NAME  = "pwa-project-cache-v0.1";
+const CACHE_NAME  = "pwa-project-cache-v0.2";
 
 const urlsToCache = [
+    "offline.html",
     "index.html",
-    "img/plume.jpg",
+    "app.js",
 ];
 
 // 残したいキャッシュのバージョンをこの配列に入れる
